2d14b964f3c3f1504c44cfe4adb9c2927c673548
RobinTang <sintrb@gmail.com>
2013-09-25 21:33:15 +0800

    Add __delitem__

--- SinKVDB.py
+++ SinKVDB.py
@@ -33,29 +33,52 @@ CREATE TABLE IF NOT EXISTS `%s` (
 ''' 
 
 class SinKVDB(object):
+    '''
+    A Python Key-Value Database.
+    '''
     def __init__(self, dbcon=None, table=None, tag=None, reset=False, debug = False):
+        '''
+        A Python Key-Value Database.
+        @dbcon Database connection
+        @table Data table name
+        @tag A tag, in most time, you use this tag to distinguish different KVDB in same table
+        @reset Reset the table or not
+        @debug In debug mode or not
+        '''
         self.dbcon = dbcon
         self.table = table
         self.tag = tag
         self.debug = debug
         self.__pingdb__()
         if reset:
-            self.reset()
+            self.reset_table()
         else:
             self.__create_table__()
         
     
     def __delete_table__(self):
+        '''
+        Delete the table
+        '''
         return self.__execsql__(__TPL_DELETETABLE__%self.table)
     
     def __create_table__(self):
+        '''
+        Create table to store key-value data
+        '''
         return self.__execsql__(__TPL_CREATETABLE__%self.table)
         
     def __pingdb__(self):
+        '''
+        Ping the database connection when the connection was die
+        '''
         self.dbcon.ping(True)
         self.dbcur = self.dbcon.cursor()
         
     def __execsql__(self, query, args=None):
+        '''
+        Execute a SQL query
+        '''
         if self.debug:
             if args is not None:
                 sql = query % self.dbcon.literal(args)
@@ -69,8 +92,11 @@ class SinKVDB(object):
             return self.dbcur.execute(query, args)
         return None
     
-    def __sql2array__(self, sql, args=None):
-        count = self.__execsql__(sql, args)
+    def __sql2array__(self, query, args=None):
+        '''
+        Execute a SQL query and switch ResultSet to Object array
+        '''
+        count = self.__execsql__(query, args)
         if count:
             names = [x[0] for x in self.dbcur.description]
             res = []
@@ -83,25 +109,43 @@ class SinKVDB(object):
         return res
     
     def add_one(self, key, value, ctype):
+        '''
+        Add record by key-value and type
+        '''
         return self.__execsql__('INSERT INTO `'+self.table+'`(`key`, `value`, `tag`, `type`, `createtime`, `modifytime`) VALUES(%s, %s, %s, %s, %s, %s)', [key, value, self.tag, ctype, time.time(), time.time()])
     
     def set_one(self, key, value, ctype):
+        '''
+        Set record by key-value and type
+        '''
         return self.__execsql__('UPDATE `'+self.table+'` SET `value`=%s, `tag`=%s, `type`=%s, `modifytime`=%s WHERE `key`=%s', [value, self.tag, ctype, time.time(), key])
     
     def get_one(self, key):
+        '''
+        Get one record by key
+        '''
         objs = self.__sql2array__('SELECT * FROM `'+self.table+'` WHERE `key`=%s and `tag`=%s LIMIT 1', [key, self.tag])
         if objs and len(objs):
             return objs[0]
         else:
             return None
     
-    def reset(self):
+    def reset_table(self):
+        '''
+        Rest the KVDB, it will clear all data store on this table
+        '''
         try:
             self.__delete_table__()
             self.__create_table__()
             return True
         except:
             return False
+    
+    def commit(self):
+        '''
+        Commit change to Database
+        '''
+        return self.dbcon.commit()
     
     def __getitem__(self, key):
         obj = self.get_one(key)
@@ -125,13 +169,21 @@ class SinKVDB(object):
             return self.add_one(key, value, ctype)
         else:
             return True
+        
+    def __delitem__(self, key):
+        return self.__execsql__('DELETE FROM `'+self.table+'` WHERE `key`=%s and `tag`=%s LIMIT 1', [key, self.tag])
 
 import unittest
 class SinKVDBTest(unittest.TestCase):
+    '''
+    Test Case of SinKVDB
+    '''
     def __init__(self, methodName):
         unittest.TestCase.__init__(self, methodName)
-        con = MySQLdb.connect(host='127.0.0.1', user='trb', passwd='123', db='dbp', port=3306)
-        self.kvdb = SinKVDB(dbcon=con, table='trb', tag='test', reset=False, debug=True)
+        if not hasattr(SinKVDBTest, 'kvdb'):
+            con = MySQLdb.connect(host='127.0.0.1', user='trb', passwd='123', db='dbp', port=3306)
+            SinKVDBTest.kvdb = SinKVDB(dbcon=con, table='tb_mykvdb', tag='test', reset=False, debug=True)
+        self.kvdb = SinKVDBTest.kvdb
         
         self.predict = {}   # Hold key-values witch will be tested.
         self.predict['bool'] = True
@@ -145,7 +197,8 @@ class SinKVDBTest(unittest.TestCase):
     def test0Store(self):
         for (k,v) in self.predict.items():
             self.kvdb[k] = v
-        self.kvdb.dbcon.commit()
+            
+        self.kvdb.commit()
     
     def test1Read(self):
         for k in self.predict.keys():
@@ -159,9 +212,29 @@ class SinKVDBTest(unittest.TestCase):
         self.assertDictEqual(self.kvdb['dict'], self.predict['dict'], 'dict not eq')
         self.assertListEqual(list(self.kvdb['list']), self.predict['list'], 'list not eq')
         self.assertListEqual(list(self.kvdb['tuple']), list(self.predict['tuple']), 'tuple not eq')
+    
+    def test3Del(self):
+        del self.kvdb['bool']
+        del self.kvdb['int']
+        del self.kvdb['str']
+        del self.kvdb['float']
+        del self.kvdb['dict']
+        del self.kvdb['list']
+        del self.kvdb['tuple']
         
+        self.kvdb.commit()
+        
+        self.assertEquals(self.kvdb['bool'], None, 'bool not eq')
+        self.assertEquals(self.kvdb['int'], None, 'int not eq')
+        self.assertEquals(self.kvdb['str'], None, 'str not eq')
+        self.assertEquals(self.kvdb['float'], None, 'float not eq')
+        self.assertEquals(self.kvdb['dict'], None, 'dict not eq')
+        self.assertEquals(self.kvdb['list'], None, 'list not eq')
+        self.assertEquals(self.kvdb['tuple'], None, 'tuple not eq')
+    
 if __name__ == '__main__':
     unittest.main()
+    SinKVDB(1, 1, 1, 1, 1)
 
     
     
