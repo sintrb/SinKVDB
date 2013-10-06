ced09be3e9ea95b2699e943ecef488e45c529c26
RobinTang <sintrb@gmail.com>
2013-10-06 16:26:33 +0800

    Unicode support test.

--- SinKVDBTest.py
+++ SinKVDBTest.py
@@ -20,6 +20,7 @@ class SinKVDBTest(unittest.TestCase):
 		self.predict = {}   # Hold key-values witch will be tested.
 		self.predict['bool'] = True
 		self.predict['str'] = 'this is string'
+		self.predict['unicode'] = u'this is string'
 		self.predict['int'] = 1
 		self.predict['float'] = 3.14159
 		self.predict['dict'] = {'name':'Robin', 'age':23}
@@ -40,6 +41,7 @@ class SinKVDBTest(unittest.TestCase):
 		self.assertEquals(self.kvdb['bool'], self.predict['bool'], 'bool not eq')
 		self.assertEquals(self.kvdb['int'], self.predict['int'], 'int not eq')
 		self.assertEquals(self.kvdb['str'], self.predict['str'], 'str not eq')
+		self.assertEquals(self.kvdb['unicode'], self.predict['unicode'], 'str not eq')
 		self.assertEquals(self.kvdb['float'], self.predict['float'], 'float not eq')
 		self.assertDictEqual(self.kvdb['dict'], self.predict['dict'], 'dict not eq')
 		self.assertListEqual(self.kvdb['list'], self.predict['list'], 'list not eq')
@@ -69,12 +71,14 @@ class SinKVDBTest(unittest.TestCase):
 		del self.kvdb['dict']
 		del self.kvdb['list']
 		del self.kvdb['tuple']
+		del self.kvdb['unicode']
 		
 		self.kvdb.commit()
 		
 		self.assertEquals(self.kvdb['bool'], None, 'bool not deleted')
 		self.assertEquals(self.kvdb['int'], None, 'int not deleted')
 		self.assertEquals(self.kvdb['str'], None, 'str not deleted')
+		self.assertEquals(self.kvdb['unicode'], None, 'unicode not deleted')
 		self.assertEquals(self.kvdb['float'], None, 'float not deleted')
 		self.assertEquals(self.kvdb['dict'], None, 'dict not deleted')
 		self.assertEquals(self.kvdb['list'], None, 'list not deleted')
